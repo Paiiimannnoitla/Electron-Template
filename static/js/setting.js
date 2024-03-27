19d1cffff31c1121b8a89c292074c5863e8766f5
Paiiimannnoitla <chih870109@gmail.com>
2024-03-27 13:23:52 +0800

    Add homepage dropmenu generation function

--- static/js/setting.js
+++ static/js/setting.js
@@ -17,7 +17,36 @@ const stInit = () =>{
 
 // Page loading Function
 pageArr['setting'] = async()=>{
-	console.log(stArr)
+	
+	// Homepage
+	const isHomepage = currPage == 'homepage'
+	if(isHomepage){
+		// Loading available homepage
+		const optArr = {}
+		const tempArr = stArr['template']
+		const keyArr = Object.keys(tempArr)
+		const valArr = Object.values(tempArr)
+		
+		for(var i=0;i<valArr.length;i++){
+			const val = valArr[i]
+			for(var a=0;a<val.length;a++){
+				const e = val[a]
+				
+				const option = `<option value='` + e + `'>` + e + `</option>`
+				val[a] = option
+			}
+			valArr[i] = valArr[i].join('')
+			optArr[keyArr[i]] = valArr[i]
+		}
+		const selArr = document.querySelectorAll('.page-dropmenu')
+		for(var i=0;i<selArr.length;i++){
+			const updateDiv = selArr[i]
+			const name = updateDiv.getAttribute('name')
+			
+			updateDiv.innerHTML = optArr[name]
+		}
+		return
+	}
 }
 // Main function
 funcArr['setting'] = async()=>{
